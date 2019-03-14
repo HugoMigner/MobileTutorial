4f5131b9780d10c2558bfeb9918c024bd82533e8
HugoMigner <hugo.migner@gmail.com>
2019-03-14 00:49:08 -0400

    First commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+	public GameObject levelButtonPrefab;
+	public GameObject levelButtonContainer;
+
+	private void Start() {
+		Sprite[] thumbnails = Resources.LoadAll<Sprite>("Levels");
+		foreach(Sprite t in thumbnails){
+			GameObject button = Instantiate(levelButtonPrefab) as GameObject;
+			button.GetComponent<Image>().sprite = t;
+			button.transform.SetParent(levelButtonContainer.transform, false);
+		}
+	}
+}
